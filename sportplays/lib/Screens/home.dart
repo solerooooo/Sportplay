b1d53e9fbdc6991f7864d7582f40fc8a17aa26ca
losooooo <147423869+losooooo@users.noreply.github.com>
2023-12-05 00:58:49 +0800

    tatau saja  nak push

--- sportplays/lib/Screens/home.dart
+++ sportplays/lib/Screens/home.dart
@@ -39,6 +39,15 @@ class _HomeState extends State<Home> {
         ),
       );
     }
+
+    if (index == 3) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Profile(passUser: widget.passUser),
+        ),
+      );
+    }
   }
 
   @override
@@ -151,43 +160,45 @@ class _HomeState extends State<Home> {
                 ),
               ),
             ),
+            const SizedBox(height: 20),
             Container(
               width: 400,
-              height: 200,
+              height: 150,
               decoration: const BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderRadius: BorderRadius.all(Radius.circular(50)),
               ),
-              child: const Padding(
-                padding: EdgeInsets.all(16.0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      "Today's News!",
+                    const Text(
+                      "Looking for a court to book?",
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                     const SizedBox(height: 10),
-                    Text(
-                      'Sports Hall Repair News Report',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: SingleChildScrollView(
-                        child: Text(
-                          'We would like to inform you that the sports hall is currently undergoing essential repairs and maintenance. This initiative is part of our ongoing efforts to enhance the overall facility and ensure a safe and enjoyable environment for everyone.',
-                          style: TextStyle(
-                            fontSize: 15,
-                          ),
-                          textAlign: TextAlign.center,
+                    SizedBox(
+                      width: 150,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const BookingPage(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.lightGreenAccent,
+                        ), // Set the button color
+                        child: const Text(
+                          'Book here',
+                          style: TextStyle(fontSize: 16),
                         ),
                       ),
                     ),
@@ -199,8 +210,12 @@ class _HomeState extends State<Home> {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         currentIndex: _selectedIndex,
         onTap: _onTabSelected,
+        selectedItemColor: Colors.black, 
+        unselectedItemColor: Colors.black.withOpacity(0.5),
+        showUnselectedLabels: true,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -209,6 +224,10 @@ class _HomeState extends State<Home> {
           BottomNavigationBarItem(
             icon: Icon(Icons.add),
             label: 'Booking',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.question_answer),
+            label: 'Q&A',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
