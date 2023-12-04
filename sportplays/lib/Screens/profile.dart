50b3fa99f47c8a691726cc8b3f05426d1139e13c
losooooo <147423869+losooooo@users.noreply.github.com>
2023-12-04 22:09:30 +0800

    Update profile.dart

--- sportplays/lib/Screens/profile.dart
+++ sportplays/lib/Screens/profile.dart
@@ -1,11 +1,11 @@
 // profile.dart
 import 'package:flutter/material.dart';
-import '../models/user.dart';
+import '../Models/user.dart';
+import 'setting.dart';
 
 class Profile extends StatefulWidget {
-  final User passUser;
-
   const Profile({Key? key, required this.passUser}) : super(key: key);
+  final User passUser;
 
   @override
   State<Profile> createState() => _ProfileState();
@@ -20,23 +20,6 @@ class _ProfileState extends State<Profile> {
     passUser = widget.passUser;
   }
 
-  Widget buildInfoRow(IconData icon, String text) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon),
-        const SizedBox(width: 10),
-        Flexible(
-          child: Text(
-            text,
-            style: const TextStyle(fontSize: 20),
-            overflow: TextOverflow.ellipsis,
-          ),
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,10 +30,10 @@ class _ProfileState extends State<Profile> {
         actions: [
           IconButton(
             onPressed: () {
-              /* MaterialPageRoute route = MaterialPageRoute(
+              MaterialPageRoute route = MaterialPageRoute(
                 builder: (context) => const Setting(),
               );
-              Navigator.push(context, route);*/
+              Navigator.push(context, route);
             },
             icon: const Icon(Icons.settings),
           ),
@@ -67,64 +50,155 @@ class _ProfileState extends State<Profile> {
               ),
               const SizedBox(height: 10),
               Text(
-                '${passUser.getName()}',
+                '${passUser.name}',
                 style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 10),
               Text(
-                '${passUser.getId()}',
-                style: const TextStyle(fontSize: 16),
+                '${passUser.email}',
+                style: const TextStyle(fontSize: 15),
               ),
               const SizedBox(height: 10),
-              Container(
-                width: 300,
-                height: 40,
-                decoration: const BoxDecoration(
-                  color: Colors.lightGreenAccent,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Active Student',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                width: 300,
-                height: 400,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment:
-                        MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const Text(
-                        'About',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, 
-                            fontSize: 20,
-                            ),
+              Column(
+                children: [
+                  Container(
+                      width: 300,
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        color: Colors.lightGreenAccent,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
                       ),
-                      buildInfoRow(Icons.person, passUser.getEmail()),
-                      buildInfoRow(Icons.phone, passUser.getPhone()),
-                      buildInfoRow(Icons.home, passUser.getAddress()),
-                      buildInfoRow(Icons.work, passUser.getGender()),
-                    ],
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Active Student',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ])),
+                  const SizedBox(height: 10),
+                  Container(
+                    width: 300,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'About',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.person),
+                            const SizedBox(width: 10),
+                            Text(
+                              '${passUser.email}',
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.phone),
+                            const SizedBox(width: 10),
+                            // Add your phone information here
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.home),
+                            const SizedBox(width: 10),
+                            // Add your address information here
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.work),
+                            const SizedBox(width: 10),
+                            // Add your job information here
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                  const SizedBox(height: 10),
+                  Container(
+                    width: 300,
+                    height: 60,
+                    decoration: const BoxDecoration(
+                      color: Colors.lightGreenAccent,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Booking Notification',
+                          style: TextStyle(fontSize: 17),
+                        ),
+                        const SizedBox(width: 10),
+                        NotiSwitch(),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 10),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class NotiSwitch extends StatefulWidget {
+  const NotiSwitch({Key? key}) : super(key: key);
+
+  @override
+  State<NotiSwitch> createState() => _NotiSwitchState();
+}
+
+class _NotiSwitchState extends State<NotiSwitch> {
+  bool light0 = true;
+  bool light1 = true;
+
+  final MaterialStateProperty<Icon?> thumbIcon =
+      MaterialStateProperty.resolveWith<Icon?>(
+    (Set<MaterialState> states) {
+      if (states.contains(MaterialState.selected)) {
+        return const Icon(Icons.check);
+      }
+      return const Icon(Icons.close);
+    },
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Switch(
+          value: light0,
+          onChanged: (bool value) {
+            setState(() {
+              light0 = value;
+            });
+          },
+        ),
+      ],
     );
   }
 }
