cb3d1c052041ab6e5b4d0090db005d84a465a666
mikacchi <103328908+FazSuraya@users.noreply.github.com>
2023-12-12 09:22:37 +0800

    add editbookingdetails, modify viewbookingdetails, setting

--- sportplays/lib/Models/bookdetails.dart
+++
@@ -1,8 +0,0 @@
-//bookdetails.dart
-/*class Book {
-  String bookingId;
-  String place;
-  String date;
-  String time;
-  
-}*/
---
+++ sportplays/lib/Models/booking.dart
@@ -0,0 +1,8 @@
+class Booking {
+  final String userName;
+  final String facility;
+  final DateTime startTime;
+  final DateTime endTime;
+
+  Booking(this.userName, this.facility, this.startTime, this.endTime);
+}
