8f8f34bd310533be69de4a352b4e172773845487
losooooo <147423869+losooooo@users.noreply.github.com>
2023-12-15 03:36:35 +0800

    Update admin availability setting

--- sportplays/lib/Models/bookingdetails.dart
+++ sportplays/lib/Models/bookingdetails.dart
@@ -5,5 +5,16 @@ class Booking {
   final DateTime startTime;
   final DateTime endTime;
 
+
   Booking(this.userName, this.facility, this.startTime, this.endTime);
+
+   static String selectedTime = ''; // Make it global
+  static Map<String, int> selectedCourts = {}; // Make it global
+
+  static void updateAvailability(String time, int pingPongCourts, int badmintonCourts, int squashCourts) {
+    selectedTime = time;
+    selectedCourts['pingPong_$time'] = pingPongCourts;
+    selectedCourts['badminton_$time'] = badmintonCourts;
+    selectedCourts['squash_$time'] = squashCourts;
+  }
 }
--- sportplays/lib/Screens/home_admin.dart
+++ sportplays/lib/Screens/home_admin.dart
@@ -8,13 +8,15 @@ import 'register.dart';
 import '../models/user.dart';
 import 'booking.dart';
 import 'qna.dart';
-import 'Availability.dart'; // Import the AvailabilityPage
+import '../Models/bookingdetails.dart';
+
 class HomeAdmin extends StatefulWidget {
   final User passUser;
-   HomeAdmin({Key? key, required this.passUser}) : super(key: key);
+  HomeAdmin({Key? key, required this.passUser}) : super(key: key);
   @override
   _HomeAdminState createState() => _HomeAdminState();
 }
+
 class _HomeAdminState extends State<HomeAdmin> {
   int _selectedIndex = 0;
   void _onTabSelected(int index) {
@@ -29,7 +31,7 @@ class _HomeAdminState extends State<HomeAdmin> {
             passUser: widget.passUser,
             selectedTime: 'YourSelectedTimeHere',
           ),
-       ),
+        ),
       );
     }
     if (index == 2) {
@@ -49,12 +51,13 @@ class _HomeAdminState extends State<HomeAdmin> {
       );
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text('HomePage (ADMIN)'),
-        backgroundColor:  Color(0xFFb364f3), // Make app bar transparent
+        title: Text('HomePage (ADMIN)'),
+        backgroundColor: Color(0xFFb364f3), // Make app bar transparent
         elevation: 0, // Remove app bar shadow
       ),
       backgroundColor:
@@ -69,8 +72,7 @@ class _HomeAdminState extends State<HomeAdmin> {
                 image: DecorationImage(
                   image: AssetImage('images/sporthall.png'),
                   colorFilter: ColorFilter.mode(
-                    Colors.black.withOpacity(
-                        0.5), 
+                    Colors.black.withOpacity(0.5),
                     BlendMode.dstATop,
                   ),
                   fit: BoxFit.cover,
@@ -82,25 +84,28 @@ class _HomeAdminState extends State<HomeAdmin> {
               ),
             ),
             ListTile(
-              title:  Text('Login'),
+              title: Text('Login'),
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) =>  Login()),
+                  MaterialPageRoute(builder: (context) => Login()),
                 );
               },
             ),
             ListTile(
-              title:  Text('Register'),
+              title: Text('Register'),
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) =>  Register(firestore: FirebaseFirestore.instance,)),
+                  MaterialPageRoute(
+                      builder: (context) => Register(
+                            firestore: FirebaseFirestore.instance,
+                          )),
                 );
               },
             ),
             ListTile(
-              title:  Text('Profile'),
+              title: Text('Profile'),
               onTap: () {
                 Navigator.push(
                   context,
@@ -109,19 +114,19 @@ class _HomeAdminState extends State<HomeAdmin> {
                 );
               },
             ),
-           ListTile(
-              title:  Text('Booking Page'),
-             onTap: () {
+            ListTile(
+              title: Text('Booking Page'),
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                       builder: (context) => BookingPage(
-                         passUser: widget.passUser,
+                          passUser: widget.passUser,
                           selectedTime: 'YourSelectedTimeHere')),
                 );
               },
-           ),
-         ],
+            ),
+          ],
         ),
       ),
       body: Container(
@@ -133,228 +138,118 @@ class _HomeAdminState extends State<HomeAdmin> {
           ),
         ),
         child: Center(
-          child: Column(
-            children: [
-               SizedBox(height: 10),
-              Text(
-                'haa kerja, ${widget.passUser.getName()}!',
-                style:  TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.white,
-                ),
+          child: Column(children: [
+            SizedBox(height: 10),
+            Text(
+              'haa kerja, ${widget.passUser.getName()}!',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
               ),
-               SizedBox(height: 10),
-              Container(
-                width: 400,
-                height: 200,
-                decoration:  BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child:  Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Today's News!",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                       SizedBox(height: 10),
-                      Text(
-                        'Sports Hall Repair News Report',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                       SizedBox(height: 10),
-                      Expanded(
-                        child: SingleChildScrollView(
-                          child: Text(
-                            'We would like to inform you that the sports hall is currently undergoing essential repairs and maintenance. This initiative is part of our ongoing efforts to enhance the overall facility and ensure a safe and enjoyable environment for everyone.',
-                            style: TextStyle(
-                              fontSize: 15,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: 400,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
               ),
-               SizedBox(height: 20),
-              // Containers for Ping Pong, Badminton, and Squash
-              Container(
-                width: 400, // Set the width to take the full available space
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    // Container for Ping Pong
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AvailabilityAdminPage(
-                              passUser: widget.passUser,
-                              sport:
-                                  'Ping Pong', // Pass the sport type to AvailabilityPage
-                            ),
-                          ),
-                        );
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 100,
-                            height: 120,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image.asset(
-                                  'images/pingpong.png',
-                                  height: 50,
-                                ),
-                                 SizedBox(height: 10),
-                                Text(
-                                  'Ping Pong',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    // Container for Badminton
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AvailabilityAdminPage(
-                              passUser: widget.passUser,
-                              sport:
-                                  'Badminton', // Pass the sport type to AvailabilityPage
-                            ),
-                          ),
-                        );
-                      },
-                      child: Column(
-                        children: [
-                          Container(
-                            width: 100,
-                            height: 120,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image.asset(
-                                  'images/badminton.png',
-                                  height: 50,
-                                ),
-                                 SizedBox(height: 10),
-                                Text(
-                                  'Badminton',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    // Container for Squash
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AvailabilityAdminPage(
-                              passUser: widget.passUser,
-                              sport:
-                                  'Squash', // Pass the sport type to AvailabilityPage
-                            ),
-                          ),
-                        );
-                      },
-                      child: Column(
-                        children: [
-                          Container(
-                            width: 100,
-                            height: 120,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image.asset(
-                                  'images/squash.png',
-                                  height: 50,
-                                ),
-                                 SizedBox(height: 10),
-                                Text(
-                                  'Squash',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-               SizedBox(height: 20),
-              Container(
-                width: 600, // Set the width to take the full available space
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset(
-                      'images/sporthall.png',
-                      height:
-                          180, // Set the height to match the other containers
-                      width: 600, // Set the width to match the other containers
+                    Text(
+                      "Today's News!",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                     SizedBox(height: 10),
+                    SizedBox(height: 10),
+                    Text(
+                      'Sports Hall Repair News Report',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Text(
+                          'We would like to inform you that the sports hall is currently undergoing essential repairs and maintenance. This initiative is part of our ongoing efforts to enhance the overall facility and ensure a safe and enjoyable environment for everyone.',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 20),
+            // Containers for Ping Pong, Badminton, and Squash
+            Container(
+              width: 300,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 10),
+                  Text(
+                    'Update the time slot',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AvailabilityAdminPage(
+                            passUser: widget.passUser,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Text('Go to Availability Page'),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+            Container(
+              width: 600, // Set the width to take the full available space
+              child: Column(
+                children: [
+                  Image.asset(
+                    'images/sporthall.png',
+                    height: 180, // Set the height to match the other containers
+                    width: 600, // Set the width to match the other containers
+                  ),
+                  SizedBox(height: 10),
+                ],
+              ),
+            ),
+          ]),
         ),
       ),
+
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: _selectedIndex,
@@ -362,7 +257,7 @@ class _HomeAdminState extends State<HomeAdmin> {
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.black.withOpacity(0.5),
         showUnselectedLabels: true,
-        items:  [
+        items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
