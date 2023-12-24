7bf676823b7eecabec4991fcf6f5d07efcaedb9a
mikacchi <103328908+FazSuraya@users.noreply.github.com>
2023-12-24 18:59:38 +0800

    stripe

--- sportplays/lib/Screens/booking.dart
+++ sportplays/lib/Screens/booking.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sportplays/Models/bookingdetails.dart';
 import 'package:sportplays/models/user.dart';
 import 'package:sportplays/Screens/availability.dart';
 import 'package:sportplays/Screens/home.dart';
 import 'package:sportplays/Screens/profile.dart';
 import 'package:sportplays/Screens/qna.dart';
+import 'package:http/http.dart' as http;
 
 class BookingPage extends StatefulWidget {
   final User passUser;
@@ -25,6 +30,7 @@ class BookingPage extends StatefulWidget {
 class _BookingPageState extends State<BookingPage> {
   late Booking booking;
   int _selectedIndex = 0;
+  Map<String, dynamic>? paymentIntent;
 
   @override
   void initState() {
@@ -40,6 +46,37 @@ class _BookingPageState extends State<BookingPage> {
     
     // Fetch the next available bookingId from Firestore
     _fetchNextBookingId();
+  }
+
+  Future<void> stripeMakePayment() async {
+    try {
+      paymentIntent = await createPaymentIntent('100', 'MYR');
+      await Stripe.instance
+          .initPaymentSheet(
+              paymentSheetParameters: SetupPaymentSheetParameters(
+                  billingDetails: BillingDetails(
+                      name: 'YOUR NAME',
+                      email: 'YOUREMAIL@gmail.com',
+                      phone: 'YOUR NUMBER',
+                      address: Address(
+                          city: 'YOUR CITY',
+                          country: 'YOUR COUNTRY',
+                          line1: 'YOUR ADDRESS 1',
+                          line2: 'YOUR ADDRESS 2',
+                          postalCode: 'YOUR PASSCODE',
+                          state: 'YOUR STATE')),
+                  paymentIntentClientSecret: paymentIntent![
+                      'client_secret'], //Gotten from payment intent
+                  style: ThemeMode.dark,
+                  merchantDisplayName: 'SPORTPLAYS',))
+          .then((value) {});
+
+      //STEP 3: Display Payment sheet
+      displayPaymentSheet();
+    } catch (e) {
+      print(e.toString());
+      Fluttertoast.showToast(msg: e.toString());
+    }
   }
 
   void _fetchNextBookingId() async {
@@ -267,7 +304,7 @@ class _BookingPageState extends State<BookingPage> {
                 const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
-                    // Implement payment logic here
+                    stripeMakePayment();
                     print(
                         'Selected Payment Method: $booking.selectedPaymentMethod');
                   },
@@ -400,3 +437,54 @@ class _BookingPageState extends State<BookingPage> {
     );
   }
 }
+
+
+
+
+
+displayPaymentSheet() async {
+    try {
+      // 3. display the payment sheet.
+      await Stripe.instance.presentPaymentSheet();
+
+      Fluttertoast.showToast(msg: 'Payment succesfully completed');
+    } on Exception catch (e) {
+      if (e is StripeException) {
+        Fluttertoast.showToast(
+            msg: 'Error from Stripe: ${e.error.localizedMessage}');
+      } else {
+        Fluttertoast.showToast(msg: 'Unforeseen error: ${e}');
+      }
+    }
+  }
+
+
+//create Payment
+  createPaymentIntent(String amount, String currency) async {
+    try {
+      //Request body
+      Map<String, dynamic> body = {
+        'amount': calculateAmount(amount),
+        'currency': currency,
+      };
+
+      //Make post request to Stripe
+      var response = await http.post(
+        Uri.parse('https://api.stripe.com/v1/payment_intents'),
+        headers: {
+          'Authorization': 'Bearer ${dotenv.env['STRIPE_SECRET']}',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body,
+      );
+      return json.decode(response.body);
+    } catch (err) {
+      throw Exception(err.toString());
+    }
+  }
+
+//calculate Amount
+  calculateAmount(String amount) {
+    final calculatedAmount = (int.parse(amount)) * 100;
+    return calculatedAmount.toString();
+  }
